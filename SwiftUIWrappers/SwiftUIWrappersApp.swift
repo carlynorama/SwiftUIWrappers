95a80af701ed91aedd883ae3dfb215bbbfd5cfc4
carlynorama <carlynorama@gmail.com>
2020-10-07 17:31:07 -0700

    Initial Commit

---
+++ SwiftUIWrappers/SwiftUIWrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWrappersApp.swift
+//  SwiftUIWrappers
+//
+//  Created by Carlyn Maw on 10/7/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
